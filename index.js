ebcf0fc4ce8a9032d926574ed448854df8f68635
John Ruddock <john.jxr@pm.me>
2022-03-23 11:58:24 -0400

    Completed Task 2: created Car constructor with prototype functions

--- index.js
+++ index.js
@@ -77,13 +77,30 @@ Person.prototype.toString = function () {
       + should add the gallons to `tank`.
     - STRETCH: Give cars ability to `.drive(distance)`. The distance driven:
         + Should cause the `odometer` to go up.
-        + Should cause the the `tank` to go down taking `milesPerGallon` into account.
+        + Should cause the the `tank` to go down taking `milesPerGallon` into account. 
     - STRETCH: A car which runs out of `fuel` while driving can't drive any more distance:
         + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
 */
 
-function Car() {
+function Car(model, milesPerGallon) {
+  this.model = model;
+  this.milesPerGallon = milesPerGallon;
+  this.tank = 0;
+  this.odometer = 0;
   
+}
+
+Car.prototype.fill = function (gallons) {
+  return this.tank += gallons;
+};
+
+Car.prototype.drive = function (distance) {
+  const totalDistance = this.tank * this.milesPerGallon;
+  if (distance <= totalDistance) {
+    this.odometer += distance;
+    this.tank -= (distance/this.milesPerGallon);
+  }
+
 }
 
 
